d133b7804392debce6fbc5070cc7bbfbedbddc55
emrekar <aliemrekaradag1@gmail.com>
2021-03-22 22:43:23 +0300

    first commit

---
+++ src/test/java/com/service/clinic/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
